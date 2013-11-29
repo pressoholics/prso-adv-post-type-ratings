68767dc6c031afd3ece48302e24c0a79844ad3b2
BENJAMIN MOODY <hireme@benjaminmoody.com>
2013-11-29 16:07:32 -0500

    Plugin main JS file to handle all ajax requests from ratings as well as init ratings on page load

--- inc/js/zg-item-ratings-script.js
+++ inc/js/zg-item-ratings-script.js
@@ -3,33 +3,44 @@
 */
 jQuery(document).ready(function( $ ){
 	
+	//Init vars
+	var ajaxRequestUrl 		= ZgItemRatingsVars.ajaxUrl;
+	var nonceValue			= ZgItemRatingsVars.ajaxNonce;
+	var rateUpdateAction	= ZgItemRatingsVars.rateUpdateAction;
+	
 	$('.zg-item-ratings-rateit').rateit({ max: 5 });
 	
 	$('.zg-item-ratings-rateit').bind('rated reset', function (e) {
 
 		var ri = $(this);
 		
-		//if the use pressed reset, it will get value: 0 (to be compatible with the HTML range control), we could check if e.type == 'reset', and then set the value to  null .
-		var value = ri.rateit('value');
-		var productID = ri.data('productid'); // if the product id was in some hidden field: ri.closest('li').find('input[name="productid"]').val()
+		//Cache rate item vars
+		var value 			= ri.rateit('value');
+		var itemID 			= ri.data('itemid'); 
+		var ratingGroupID	= ri.data('ratinggroupid');
 		
 		//maybe we want to disable voting?
 		ri.rateit('readonly', true);
 		
-		/*
+		//Make ajax request to update item rating
 		$.ajax({
-			url: 'rateit.aspx', //your server side script
-			data: { id: productID, value: value }, //our data
+			url: ajaxRequestUrl,
+			data: { 
+				action: rateUpdateAction,
+				zgItemRateNonce: nonceValue,
+				itemID: itemID, 
+				rateValue: value,
+				ratingGroup: ratingGroupID
+			},
 			type: 'POST',
 			success: function (data) {
-			 $('#response').append('<li>' + data + '</li>');
 			
 			},
 			error: function (jxhr, msg, err) {
-			 $('#response').append('<li style="color:red">' + msg + '</li>');
+			
 			}
 		});
-		*/
+		
 		
 	});
 	
