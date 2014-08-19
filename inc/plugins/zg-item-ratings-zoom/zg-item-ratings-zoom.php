b4625ce117d6e22a3db8afe84f4bf497313b4176
BENJAMIN MOODY <hireme@benjaminmoody.com>
2014-08-19 12:18:47 -0400

    no message

--- inc/plugins/zg-item-ratings-zoom/zg-item-ratings-zoom.php
+++ inc/plugins/zg-item-ratings-zoom/zg-item-ratings-zoom.php
@@ -1,34 +1,10 @@
 <?php
-/*
- * Plugin Name: Item Ratings Image Zoom by Benjamin Moody & Zeitguys Inc.
- * Plugin URI: http://www.BenjaminMoody.com & http://www.zeitguys.com
- * Description: 
- * Author: Benjamin Moody
- * Version: 1.0
- * Author URI: http://www.BenjaminMoody.com
- * License: GPL2+
- * Text Domain: zg_item_ratings_zoom
- * Domain Path: /languages/
- */
 
 //Define plugin constants
-define( 'ZGITEMRATINGSZOOM__MINIMUM_WP_VERSION', '3.0' );
 define( 'ZGITEMRATINGSZOOM__VERSION', '1.0' );
 define( 'ZGITEMRATINGSZOOM__DOMAIN', 'zg-item-ratings-zoom-plugin' );
 define( 'ZGITEMRATINGSZOOM__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ZGITEMRATINGSZOOM__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
-//Set Activation/Deactivation hooks
-register_activation_hook( __FILE__, 'zg_item_ratings_zoom_activation' );
-register_deactivation_hook( __FILE__, 'zg_item_ratings_zoom_deactivation' );
-
-function zg_item_ratings_zoom_activation( $network_wide = NULL ) {
-	
-}
-
-function zg_item_ratings_zoom_deactivation() {
-	
-}
 
 
 //Init plugin during 'admin_init' action to ensure parent is instatiated
